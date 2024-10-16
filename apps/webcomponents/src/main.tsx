9cb7a8ae67edbc1cee3c13ac69857e7139cd6518
Francois Prunayre <fx.prunayre@gmail.com>
2024-10-16 10:03:50 +0200

    Webcomponent / Attach style to shadow DOM.

--- apps/webcomponents/src/main.tsx
+++ apps/webcomponents/src/main.tsx
@@ -4,31 +4,61 @@ import * as ReactDOM from 'react-dom';
 import reactToWebComponent from 'react-to-webcomponent';
 import { UiSearch } from '@catalogue/ui/search';
 import SearchResultsTableWc from './app/search-results-table-wc/search-results-table-wc';
-import SearchResultsCardWc from "./app/search-results-card-wc/search-results-card-wc";
-import SearchResultsGraphWc from "./app/search-results-graph-wc/search-results-graph-wc";
+import SearchResultsCardWc from './app/search-results-card-wc/search-results-card-wc';
+import SearchResultsGraphWc from './app/search-results-graph-wc/search-results-graph-wc';
 
-const catalogueSearch = reactToWebComponent(UiSearch, React, ReactDOM, {
-  shadow: true,
+export const convertReact2WebComponent = (
+  Component: Parameters<typeof reactToWebComponent>[0],
+  options?: Parameters<typeof reactToWebComponent>[1]
+) => {
+  const WebComponent = reactToWebComponent(Component, React, ReactDOM, options);
+
+  class WebComponentWithStyle extends WebComponent {
+    connectedCallback() {
+      super.connectedCallback();
+      ['main.css', 'styles.css'].forEach((style) => {
+        const link = document.createElement('link');
+        link.setAttribute('rel', 'stylesheet');
+        link.setAttribute('href', `${style}`);
+        // link.setAttribute("href", `https://metawal.wallonie.be/geonetwork/catalog/lib/icho/${style}`);
+        // @ts-ignore
+        this.shadowRoot?.append(link);
+      });
+    }
+  }
+  return WebComponentWithStyle;
+};
+
+const catalogueSearch = convertReact2WebComponent(UiSearch, {
+  shadow: 'open',
 });
+// @ts-ignore
 customElements.define('catalogue-search', catalogueSearch);
 
-const searchResultTableReactivelist = reactToWebComponent(
+const searchResultTableReactivelist = convertReact2WebComponent(
   SearchResultsTableWc,
-  React,
-  ReactDOM
+  {
+    shadow: 'open',
+  }
 );
+
+// @ts-ignore
 customElements.define('catalogue-results-table', searchResultTableReactivelist);
 
-const searchResultCardReactivelist = reactToWebComponent(
+const searchResultCardReactivelist = convertReact2WebComponent(
   SearchResultsCardWc,
-  React,
-  ReactDOM
+  {
+    shadow: 'open',
+  }
 );
+// @ts-ignore
 customElements.define('catalogue-results-card', searchResultCardReactivelist);
 
-const searchResultGraphReactivelist = reactToWebComponent(
+const searchResultGraphReactivelist = convertReact2WebComponent(
   SearchResultsGraphWc,
-  React,
-  ReactDOM
+  {
+    shadow: 'open',
+  }
 );
+// @ts-ignore
 customElements.define('catalogue-results-graph', searchResultGraphReactivelist);
